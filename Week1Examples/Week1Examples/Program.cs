796141b71c4c68086979daa98ab03efa2fab1c64
Eric Charnesky <eric.charnesky@careevolution.com>
2017-01-12 19:21:35 -0500

    Guess a number game

--- Week1Examples/Week1Examples/Program.cs
+++ Week1Examples/Week1Examples/Program.cs
@@ -10,6 +10,30 @@ namespace Week1Examples
 	{
 		static void Main( string[] args )
 		{
+			Random random = new Random();
+			int magicNumber = random.Next( 1, 100 );
+			int usersGuess = 0;
+			int numberOfGuess = 0;
+			while( usersGuess != magicNumber )
+			{
+				Console.WriteLine( "Guess a number 1-100" );
+				usersGuess = Int32.Parse(Console.ReadLine());
+				numberOfGuess++;
+
+				if ( usersGuess > magicNumber )
+				{
+					Console.WriteLine( "Too high, guess again!" );
+				}
+				else if ( usersGuess < magicNumber )
+				{
+					Console.WriteLine( "Too low, guess again!" );
+				}
+				else
+				{
+					Console.WriteLine( "You guessed it in " + numberOfGuess + " guesses!" );
+				}
+			}
+			Console.ReadLine();
 		}
 	}
 }
