8ab0e4d78217521cf5af4cd908e43b080dbf5e48
Eric Charnesky <eric.charnesky@careevolution.com>
2017-01-19 19:25:10 -0500

    Shifting

--- MoreExamplesFrom4-9/MoreExamplesFrom4-9/Form1.cs
+++ MoreExamplesFrom4-9/MoreExamplesFrom4-9/Form1.cs
@@ -12,15 +12,28 @@ namespace MoreExamplesFrom4_9
 
 		private void button1_Click( object sender, EventArgs e )
 		{
+			Shift();
+		}
+
+		private void Shift()
+		{
+			int valueToShiftBy = Convert.ToInt32( textBox2.Text );
 			string startingText = textBox1.Text;
 			char[] letters = startingText.ToCharArray();
 			for ( int index = 0; index < letters.Length; index++ )
 			{
 				char letter = letters[ index ];
 				int unicodeValue = (int)letter;
-				unicodeValue += 5;
+				unicodeValue += valueToShiftBy;
 				letters[ index ] = (char)unicodeValue;
 			}
+
+			// this won't work - letter can't be changed
+			//foreach ( char letter in letters )
+			//{
+			//	letter += valueToShiftBy;
+			//}
+
 			label1.Text = new string( letters );
 		}
 	}
