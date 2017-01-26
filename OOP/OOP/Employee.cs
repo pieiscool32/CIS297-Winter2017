f48bb2f24dcd354a76ff9fbf576e1f3cc9b4252a
Eric Charnesky <eric.charnesky@careevolution.com>
2017-01-26 18:54:02 -0500

    Classes, override and casting

---
+++ OOP/OOP/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+	public class Employee
+	{
+		private string _name;
+		private int _idNumber;
+		private double _salary;
+
+		public Employee(string name, int idNumber, double salary)
+		{
+			_name = name;
+			_idNumber = idNumber;
+			_salary = salary;
+		}
+
+		public string getName()
+		{
+			return _name;
+		}
+
+		public int getIDNumber()
+		{
+			return _idNumber;
+		}
+
+		public double getSalary()
+		{
+			return _salary;
+		}
+
+		public override string ToString()
+		{
+			return $"Name: {getName()} - ID: {getIDNumber()} - Salary: ${getSalary()}";
+		}
+	}
+}
--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -10,6 +10,42 @@ namespace OOP
 	{
 		static void Main( string[] args )
 		{
+			Employee dan = new Employee( "Dan V.", 123, 100000.0 );
+			PrintEmployee( dan );
+			Employee eddie = new Employee( "Eddie U.", 234, 1000000 );
+			PrintEmployee( eddie );
+			Employee eric = null;
+			PrintEmployee( eric );
+
+			object[] items = new object[ 3 ];
+			items[ 0 ] = dan;
+			items[ 1 ] = eddie;
+			items[ 2 ] = "test";
+
+			for ( int index = 0; index< items.Length; index++ )
+			{
+				PrintEmployee( items[ index ] );
+			}
+
+			Console.ReadKey();
+		}
+
+		static void PrintEmployee( object obj )
+		{
+			// this one crashes if it can't cast
+			// Employee employee = (Employee)obj; 
+
+			// this equals null if it can't cast
+			Employee employee = obj as Employee;
+			PrintEmployee( employee );
+		}
+
+		static void PrintEmployee( Employee employee )
+		{
+			if ( employee != null )
+			{
+				Console.WriteLine( employee.ToString() );
+			}
 		}
 	}
 }
