f5c5874b5214aea0c13455714de93ab7a6e2cdf7
Eric Charnesky <eric.charnesky@careevolution.com>
2017-01-26 20:11:30 -0500

    Unit Tests for Employee

---
+++ OOP/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOP;
+
+namespace UnitTestProject1
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestEmployeeConstructorDefaultName()
+		{
+			Employee testEmployee = new Employee( "", 100000 );
+			Assert.AreEqual( "Not Assigned", testEmployee.Name );
+			Assert.AreEqual( 100000, testEmployee.AnnualSalary );
+		}
+
+		[TestMethod]
+		public void TestEmployeeConstructor()
+		{
+			Employee testEmployee = new Employee( "Eric", 100000 );
+			Assert.AreEqual( "Eric", testEmployee.Name );
+			Assert.AreEqual( 100000, testEmployee.AnnualSalary );
+		}
+	}
+}
