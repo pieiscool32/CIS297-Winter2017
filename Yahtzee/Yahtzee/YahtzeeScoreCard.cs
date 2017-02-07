1ad2200e3513723633417f5eb7fc4547862cce7e
Eric Charnesky <eric.charnesky@careevolution.com>
2017-02-07 13:30:34 -0500

    Finish game logic

--- Yahtzee/Yahtzee/YahtzeeScoreCard.cs
+++ Yahtzee/Yahtzee/YahtzeeScoreCard.cs
@@ -12,12 +12,15 @@ namespace Yahtzee
 		public static int SMALL_STRAIGHT_SCORE = 30;
 		public static int LARGE_STRAIGHT_SCORE = 40;
 		public static int YAHTZEE_SCORE = 50;
+		public static int UPPER_BONUS = 35;
+
 		public int Ones { get; set; } = 0;
 		public int Twos { get; set; } = 0;
 		public int Threes { get; set; } = 0;
 		public int Fours { get; set; } = 0;
 		public int Fives { get; set; } = 0;
 		public int Sixes { get; set; } = 0;
+		public int Bonus => Ones + Twos + Threes + Fours + Fives + Sixes >= 63 ? UPPER_BONUS : 0;
 		public int ThreeOfAKind { get; set; } = 0;
 		public int FourOfAKind { get; set; } = 0;
 		public int FullHouse { get; set; } = 0;
@@ -25,5 +28,8 @@ namespace Yahtzee
 		public int LargeStraight { get; set; } = 0;
 		public int Yahtzee { get; set; } = 0;
 		public int Chance { get; set; } = 0;
+		public int UpperTotal => Ones + Twos + Threes + Fours + Fives + Sixes + Bonus;
+		public int LowerTotal => ThreeOfAKind + FourOfAKind + FullHouse + SmallStraight + LargeStraight + Yahtzee + Chance;
+		public int GrandTotal => UpperTotal + LowerTotal;
 	}
 }
--- Yahtzee/Yahtzee/YahtzeeUI.Designer.cs
+++ Yahtzee/Yahtzee/YahtzeeUI.Designer.cs
@@ -58,7 +58,34 @@
 			this.twosScoreLabel = new System.Windows.Forms.Label();
 			this.threesScoreLabel = new System.Windows.Forms.Label();
 			this.scoreTwosButton = new System.Windows.Forms.Button();
-			this.scoresThreeButton = new System.Windows.Forms.Button();
+			this.scoreThreeButton = new System.Windows.Forms.Button();
+			this.foursScoreLabel = new System.Windows.Forms.Label();
+			this.fivesScoreLabel = new System.Windows.Forms.Label();
+			this.sixesScoreLabel = new System.Windows.Forms.Label();
+			this.bonusScoreLabel = new System.Windows.Forms.Label();
+			this.threeOfAKindScoreLabel = new System.Windows.Forms.Label();
+			this.fourOfAKindScoreLabel = new System.Windows.Forms.Label();
+			this.fullHouseScoreLabel = new System.Windows.Forms.Label();
+			this.smallStraightScoreLabel = new System.Windows.Forms.Label();
+			this.largeStraightScoreLabel = new System.Windows.Forms.Label();
+			this.yahtzeeScoreLabel = new System.Windows.Forms.Label();
+			this.chanceScoreLabel = new System.Windows.Forms.Label();
+			this.upperScoreLabel = new System.Windows.Forms.Label();
+			this.lowerScoreLabel = new System.Windows.Forms.Label();
+			this.grandTotalScoreLabel = new System.Windows.Forms.Label();
+			this.scoreFoursButton = new System.Windows.Forms.Button();
+			this.scoreFivesButton = new System.Windows.Forms.Button();
+			this.scoreSixesButton = new System.Windows.Forms.Button();
+			this.scoreThreeOfAKindButton = new System.Windows.Forms.Button();
+			this.scoreFourOfAKindButton = new System.Windows.Forms.Button();
+			this.scoreFullHouseButton = new System.Windows.Forms.Button();
+			this.scoreSmallStraightButton = new System.Windows.Forms.Button();
+			this.scoreLargeStraightButton = new System.Windows.Forms.Button();
+			this.scoreYahtzeeButton = new System.Windows.Forms.Button();
+			this.scoreChanceButton = new System.Windows.Forms.Button();
+			this.dieThreeHolding = new System.Windows.Forms.Label();
+			this.dieFourHolding = new System.Windows.Forms.Label();
+			this.dieFiveHolding = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.die5PictureBox)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.die4PictureBox)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.die3PictureBox)).BeginInit();
@@ -84,6 +111,7 @@
 			this.die5PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.die5PictureBox.TabIndex = 4;
 			this.die5PictureBox.TabStop = false;
+			this.die5PictureBox.Click += new System.EventHandler(this.die5PictureBox_Click);
 			// 
 			// die4PictureBox
 			// 
@@ -93,6 +121,7 @@
 			this.die4PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.die4PictureBox.TabIndex = 3;
 			this.die4PictureBox.TabStop = false;
+			this.die4PictureBox.Click += new System.EventHandler(this.die4PictureBox_Click);
 			// 
 			// die3PictureBox
 			// 
@@ -102,6 +131,7 @@
 			this.die3PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
 			this.die3PictureBox.TabIndex = 2;
 			this.die3PictureBox.TabStop = false;
+			this.die3PictureBox.Click += new System.EventHandler(this.die3PictureBox_Click);
 			// 
 			// die2PictureBox
 			// 
@@ -234,7 +264,7 @@
 			// label13
 			// 
 			this.label13.AutoSize = true;
-			this.label13.Location = new System.Drawing.Point(1343, 759);
+			this.label13.Location = new System.Drawing.Point(1343, 751);
 			this.label13.Name = "label13";
 			this.label13.Size = new System.Drawing.Size(98, 25);
 			this.label13.TabIndex = 18;
@@ -316,7 +346,7 @@
 			// twosScoreLabel
 			// 
 			this.twosScoreLabel.AutoSize = true;
-			this.twosScoreLabel.Location = new System.Drawing.Point(1503, 105);
+			this.twosScoreLabel.Location = new System.Drawing.Point(1553, 105);
 			this.twosScoreLabel.Name = "twosScoreLabel";
 			this.twosScoreLabel.Size = new System.Drawing.Size(0, 25);
 			this.twosScoreLabel.TabIndex = 27;
@@ -324,7 +354,7 @@
 			// threesScoreLabel
 			// 
 			this.threesScoreLabel.AutoSize = true;
-			this.threesScoreLabel.Location = new System.Drawing.Point(1495, 162);
+			this.threesScoreLabel.Location = new System.Drawing.Point(1553, 162);
 			this.threesScoreLabel.Name = "threesScoreLabel";
 			this.threesScoreLabel.Size = new System.Drawing.Size(0, 25);
 			this.threesScoreLabel.TabIndex = 28;
@@ -339,22 +369,285 @@
 			this.scoreTwosButton.UseVisualStyleBackColor = true;
 			this.scoreTwosButton.Click += new System.EventHandler(this.scoreTwosButton_Click);
 			// 
-			// scoresThreeButton
+			// scoreThreeButton
 			// 
-			this.scoresThreeButton.Location = new System.Drawing.Point(1693, 162);
-			this.scoresThreeButton.Name = "scoresThreeButton";
-			this.scoresThreeButton.Size = new System.Drawing.Size(140, 36);
-			this.scoresThreeButton.TabIndex = 30;
-			this.scoresThreeButton.Text = "SaveScore";
-			this.scoresThreeButton.UseVisualStyleBackColor = true;
-			this.scoresThreeButton.Click += new System.EventHandler(this.scoresThreeButton_Click);
+			this.scoreThreeButton.Location = new System.Drawing.Point(1693, 162);
+			this.scoreThreeButton.Name = "scoreThreeButton";
+			this.scoreThreeButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreThreeButton.TabIndex = 30;
+			this.scoreThreeButton.Text = "SaveScore";
+			this.scoreThreeButton.UseVisualStyleBackColor = true;
+			this.scoreThreeButton.Click += new System.EventHandler(this.scoresThreeButton_Click);
+			// 
+			// foursScoreLabel
+			// 
+			this.foursScoreLabel.AutoSize = true;
+			this.foursScoreLabel.Location = new System.Drawing.Point(1558, 222);
+			this.foursScoreLabel.Name = "foursScoreLabel";
+			this.foursScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.foursScoreLabel.TabIndex = 31;
+			// 
+			// fivesScoreLabel
+			// 
+			this.fivesScoreLabel.AutoSize = true;
+			this.fivesScoreLabel.Location = new System.Drawing.Point(1558, 280);
+			this.fivesScoreLabel.Name = "fivesScoreLabel";
+			this.fivesScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.fivesScoreLabel.TabIndex = 32;
+			// 
+			// sixesScoreLabel
+			// 
+			this.sixesScoreLabel.AutoSize = true;
+			this.sixesScoreLabel.Location = new System.Drawing.Point(1558, 342);
+			this.sixesScoreLabel.Name = "sixesScoreLabel";
+			this.sixesScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.sixesScoreLabel.TabIndex = 33;
+			// 
+			// bonusScoreLabel
+			// 
+			this.bonusScoreLabel.AutoSize = true;
+			this.bonusScoreLabel.Location = new System.Drawing.Point(1558, 404);
+			this.bonusScoreLabel.Name = "bonusScoreLabel";
+			this.bonusScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.bonusScoreLabel.TabIndex = 34;
+			// 
+			// threeOfAKindScoreLabel
+			// 
+			this.threeOfAKindScoreLabel.AutoSize = true;
+			this.threeOfAKindScoreLabel.Location = new System.Drawing.Point(1563, 463);
+			this.threeOfAKindScoreLabel.Name = "threeOfAKindScoreLabel";
+			this.threeOfAKindScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.threeOfAKindScoreLabel.TabIndex = 35;
+			// 
+			// fourOfAKindScoreLabel
+			// 
+			this.fourOfAKindScoreLabel.AutoSize = true;
+			this.fourOfAKindScoreLabel.Location = new System.Drawing.Point(1558, 526);
+			this.fourOfAKindScoreLabel.Name = "fourOfAKindScoreLabel";
+			this.fourOfAKindScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.fourOfAKindScoreLabel.TabIndex = 36;
+			// 
+			// fullHouseScoreLabel
+			// 
+			this.fullHouseScoreLabel.AutoSize = true;
+			this.fullHouseScoreLabel.Location = new System.Drawing.Point(1558, 583);
+			this.fullHouseScoreLabel.Name = "fullHouseScoreLabel";
+			this.fullHouseScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.fullHouseScoreLabel.TabIndex = 37;
+			// 
+			// smallStraightScoreLabel
+			// 
+			this.smallStraightScoreLabel.AutoSize = true;
+			this.smallStraightScoreLabel.Location = new System.Drawing.Point(1558, 640);
+			this.smallStraightScoreLabel.Name = "smallStraightScoreLabel";
+			this.smallStraightScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.smallStraightScoreLabel.TabIndex = 38;
+			// 
+			// largeStraightScoreLabel
+			// 
+			this.largeStraightScoreLabel.AutoSize = true;
+			this.largeStraightScoreLabel.Location = new System.Drawing.Point(1558, 694);
+			this.largeStraightScoreLabel.Name = "largeStraightScoreLabel";
+			this.largeStraightScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.largeStraightScoreLabel.TabIndex = 39;
+			// 
+			// yahtzeeScoreLabel
+			// 
+			this.yahtzeeScoreLabel.AutoSize = true;
+			this.yahtzeeScoreLabel.Location = new System.Drawing.Point(1558, 759);
+			this.yahtzeeScoreLabel.Name = "yahtzeeScoreLabel";
+			this.yahtzeeScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.yahtzeeScoreLabel.TabIndex = 40;
+			// 
+			// chanceScoreLabel
+			// 
+			this.chanceScoreLabel.AutoSize = true;
+			this.chanceScoreLabel.Location = new System.Drawing.Point(1558, 807);
+			this.chanceScoreLabel.Name = "chanceScoreLabel";
+			this.chanceScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.chanceScoreLabel.TabIndex = 41;
+			// 
+			// upperScoreLabel
+			// 
+			this.upperScoreLabel.AutoSize = true;
+			this.upperScoreLabel.Location = new System.Drawing.Point(1558, 886);
+			this.upperScoreLabel.Name = "upperScoreLabel";
+			this.upperScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.upperScoreLabel.TabIndex = 42;
+			// 
+			// lowerScoreLabel
+			// 
+			this.lowerScoreLabel.AutoSize = true;
+			this.lowerScoreLabel.Location = new System.Drawing.Point(1558, 941);
+			this.lowerScoreLabel.Name = "lowerScoreLabel";
+			this.lowerScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.lowerScoreLabel.TabIndex = 43;
+			// 
+			// grandTotalScoreLabel
+			// 
+			this.grandTotalScoreLabel.AutoSize = true;
+			this.grandTotalScoreLabel.Location = new System.Drawing.Point(1553, 998);
+			this.grandTotalScoreLabel.Name = "grandTotalScoreLabel";
+			this.grandTotalScoreLabel.Size = new System.Drawing.Size(0, 25);
+			this.grandTotalScoreLabel.TabIndex = 44;
+			// 
+			// scoreFoursButton
+			// 
+			this.scoreFoursButton.Location = new System.Drawing.Point(1693, 222);
+			this.scoreFoursButton.Name = "scoreFoursButton";
+			this.scoreFoursButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreFoursButton.TabIndex = 45;
+			this.scoreFoursButton.Text = "SaveScore";
+			this.scoreFoursButton.UseVisualStyleBackColor = true;
+			this.scoreFoursButton.Click += new System.EventHandler(this.scoreFoursButton_Click);
+			// 
+			// scoreFivesButton
+			// 
+			this.scoreFivesButton.Location = new System.Drawing.Point(1693, 281);
+			this.scoreFivesButton.Name = "scoreFivesButton";
+			this.scoreFivesButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreFivesButton.TabIndex = 46;
+			this.scoreFivesButton.Text = "SaveScore";
+			this.scoreFivesButton.UseVisualStyleBackColor = true;
+			this.scoreFivesButton.Click += new System.EventHandler(this.scoreFivesButton_Click);
+			// 
+			// scoreSixesButton
+			// 
+			this.scoreSixesButton.Location = new System.Drawing.Point(1693, 342);
+			this.scoreSixesButton.Name = "scoreSixesButton";
+			this.scoreSixesButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreSixesButton.TabIndex = 47;
+			this.scoreSixesButton.Text = "SaveScore";
+			this.scoreSixesButton.UseVisualStyleBackColor = true;
+			this.scoreSixesButton.Click += new System.EventHandler(this.scoreSixesButton_Click);
+			// 
+			// scoreThreeOfAKindButton
+			// 
+			this.scoreThreeOfAKindButton.Location = new System.Drawing.Point(1693, 468);
+			this.scoreThreeOfAKindButton.Name = "scoreThreeOfAKindButton";
+			this.scoreThreeOfAKindButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreThreeOfAKindButton.TabIndex = 48;
+			this.scoreThreeOfAKindButton.Text = "SaveScore";
+			this.scoreThreeOfAKindButton.UseVisualStyleBackColor = true;
+			this.scoreThreeOfAKindButton.Click += new System.EventHandler(this.scoreThreeOfAKindButton_Click);
+			// 
+			// scoreFourOfAKindButton
+			// 
+			this.scoreFourOfAKindButton.Location = new System.Drawing.Point(1693, 526);
+			this.scoreFourOfAKindButton.Name = "scoreFourOfAKindButton";
+			this.scoreFourOfAKindButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreFourOfAKindButton.TabIndex = 49;
+			this.scoreFourOfAKindButton.Text = "SaveScore";
+			this.scoreFourOfAKindButton.UseVisualStyleBackColor = true;
+			this.scoreFourOfAKindButton.Click += new System.EventHandler(this.scoreFourOfAKindButton_Click);
+			// 
+			// scoreFullHouseButton
+			// 
+			this.scoreFullHouseButton.Location = new System.Drawing.Point(1693, 583);
+			this.scoreFullHouseButton.Name = "scoreFullHouseButton";
+			this.scoreFullHouseButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreFullHouseButton.TabIndex = 50;
+			this.scoreFullHouseButton.Text = "SaveScore";
+			this.scoreFullHouseButton.UseVisualStyleBackColor = true;
+			this.scoreFullHouseButton.Click += new System.EventHandler(this.scoreFullHouseButton_Click);
+			// 
+			// scoreSmallStraightButton
+			// 
+			this.scoreSmallStraightButton.Location = new System.Drawing.Point(1693, 640);
+			this.scoreSmallStraightButton.Name = "scoreSmallStraightButton";
+			this.scoreSmallStraightButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreSmallStraightButton.TabIndex = 51;
+			this.scoreSmallStraightButton.Text = "SaveScore";
+			this.scoreSmallStraightButton.UseVisualStyleBackColor = true;
+			this.scoreSmallStraightButton.Click += new System.EventHandler(this.scoreSmallStraightButton_Click);
+			// 
+			// scoreLargeStraightButton
+			// 
+			this.scoreLargeStraightButton.Location = new System.Drawing.Point(1693, 694);
+			this.scoreLargeStraightButton.Name = "scoreLargeStraightButton";
+			this.scoreLargeStraightButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreLargeStraightButton.TabIndex = 52;
+			this.scoreLargeStraightButton.Text = "SaveScore";
+			this.scoreLargeStraightButton.UseVisualStyleBackColor = true;
+			this.scoreLargeStraightButton.Click += new System.EventHandler(this.scoreLargeStraightButton_Click);
+			// 
+			// scoreYahtzeeButton
+			// 
+			this.scoreYahtzeeButton.Location = new System.Drawing.Point(1693, 751);
+			this.scoreYahtzeeButton.Name = "scoreYahtzeeButton";
+			this.scoreYahtzeeButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreYahtzeeButton.TabIndex = 53;
+			this.scoreYahtzeeButton.Text = "SaveScore";
+			this.scoreYahtzeeButton.UseVisualStyleBackColor = true;
+			this.scoreYahtzeeButton.Click += new System.EventHandler(this.scoreYahtzeeButton_Click);
+			// 
+			// scoreChanceButton
+			// 
+			this.scoreChanceButton.Location = new System.Drawing.Point(1693, 807);
+			this.scoreChanceButton.Name = "scoreChanceButton";
+			this.scoreChanceButton.Size = new System.Drawing.Size(140, 36);
+			this.scoreChanceButton.TabIndex = 54;
+			this.scoreChanceButton.Text = "SaveScore";
+			this.scoreChanceButton.UseVisualStyleBackColor = true;
+			this.scoreChanceButton.Click += new System.EventHandler(this.scoreChanceButton_Click);
+			// 
+			// dieThreeHolding
+			// 
+			this.dieThreeHolding.AutoSize = true;
+			this.dieThreeHolding.Location = new System.Drawing.Point(620, 292);
+			this.dieThreeHolding.Name = "dieThreeHolding";
+			this.dieThreeHolding.Size = new System.Drawing.Size(0, 25);
+			this.dieThreeHolding.TabIndex = 55;
+			// 
+			// dieFourHolding
+			// 
+			this.dieFourHolding.AutoSize = true;
+			this.dieFourHolding.Location = new System.Drawing.Point(831, 292);
+			this.dieFourHolding.Name = "dieFourHolding";
+			this.dieFourHolding.Size = new System.Drawing.Size(0, 25);
+			this.dieFourHolding.TabIndex = 56;
+			// 
+			// dieFiveHolding
+			// 
+			this.dieFiveHolding.AutoSize = true;
+			this.dieFiveHolding.Location = new System.Drawing.Point(1052, 292);
+			this.dieFiveHolding.Name = "dieFiveHolding";
+			this.dieFiveHolding.Size = new System.Drawing.Size(0, 25);
+			this.dieFiveHolding.TabIndex = 57;
 			// 
 			// YahtzeeUI
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1902, 1065);
-			this.Controls.Add(this.scoresThreeButton);
+			this.Controls.Add(this.dieFiveHolding);
+			this.Controls.Add(this.dieFourHolding);
+			this.Controls.Add(this.dieThreeHolding);
+			this.Controls.Add(this.scoreChanceButton);
+			this.Controls.Add(this.scoreYahtzeeButton);
+			this.Controls.Add(this.scoreLargeStraightButton);
+			this.Controls.Add(this.scoreSmallStraightButton);
+			this.Controls.Add(this.scoreFullHouseButton);
+			this.Controls.Add(this.scoreFourOfAKindButton);
+			this.Controls.Add(this.scoreThreeOfAKindButton);
+			this.Controls.Add(this.scoreSixesButton);
+			this.Controls.Add(this.scoreFivesButton);
+			this.Controls.Add(this.scoreFoursButton);
+			this.Controls.Add(this.grandTotalScoreLabel);
+			this.Controls.Add(this.lowerScoreLabel);
+			this.Controls.Add(this.upperScoreLabel);
+			this.Controls.Add(this.chanceScoreLabel);
+			this.Controls.Add(this.yahtzeeScoreLabel);
+			this.Controls.Add(this.largeStraightScoreLabel);
+			this.Controls.Add(this.smallStraightScoreLabel);
+			this.Controls.Add(this.fullHouseScoreLabel);
+			this.Controls.Add(this.fourOfAKindScoreLabel);
+			this.Controls.Add(this.threeOfAKindScoreLabel);
+			this.Controls.Add(this.bonusScoreLabel);
+			this.Controls.Add(this.sixesScoreLabel);
+			this.Controls.Add(this.fivesScoreLabel);
+			this.Controls.Add(this.foursScoreLabel);
+			this.Controls.Add(this.scoreThreeButton);
 			this.Controls.Add(this.scoreTwosButton);
 			this.Controls.Add(this.threesScoreLabel);
 			this.Controls.Add(this.twosScoreLabel);
@@ -429,7 +722,34 @@
 		private System.Windows.Forms.Label twosScoreLabel;
 		private System.Windows.Forms.Label threesScoreLabel;
 		private System.Windows.Forms.Button scoreTwosButton;
-		private System.Windows.Forms.Button scoresThreeButton;
+		private System.Windows.Forms.Button scoreThreeButton;
+		private System.Windows.Forms.Label foursScoreLabel;
+		private System.Windows.Forms.Label fivesScoreLabel;
+		private System.Windows.Forms.Label sixesScoreLabel;
+		private System.Windows.Forms.Label bonusScoreLabel;
+		private System.Windows.Forms.Label threeOfAKindScoreLabel;
+		private System.Windows.Forms.Label fourOfAKindScoreLabel;
+		private System.Windows.Forms.Label fullHouseScoreLabel;
+		private System.Windows.Forms.Label smallStraightScoreLabel;
+		private System.Windows.Forms.Label largeStraightScoreLabel;
+		private System.Windows.Forms.Label yahtzeeScoreLabel;
+		private System.Windows.Forms.Label chanceScoreLabel;
+		private System.Windows.Forms.Label upperScoreLabel;
+		private System.Windows.Forms.Label lowerScoreLabel;
+		private System.Windows.Forms.Label grandTotalScoreLabel;
+		private System.Windows.Forms.Button scoreFoursButton;
+		private System.Windows.Forms.Button scoreFivesButton;
+		private System.Windows.Forms.Button scoreSixesButton;
+		private System.Windows.Forms.Button scoreThreeOfAKindButton;
+		private System.Windows.Forms.Button scoreFourOfAKindButton;
+		private System.Windows.Forms.Button scoreFullHouseButton;
+		private System.Windows.Forms.Button scoreSmallStraightButton;
+		private System.Windows.Forms.Button scoreLargeStraightButton;
+		private System.Windows.Forms.Button scoreYahtzeeButton;
+		private System.Windows.Forms.Button scoreChanceButton;
+		private System.Windows.Forms.Label dieThreeHolding;
+		private System.Windows.Forms.Label dieFourHolding;
+		private System.Windows.Forms.Label dieFiveHolding;
 	}
 }
 
