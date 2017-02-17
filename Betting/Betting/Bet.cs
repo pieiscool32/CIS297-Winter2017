ebf2038b96d8b2f5d51353a5591e67a187916580
Eric Charnesky <eric.charnesky@careevolution.com>
2017-02-16 20:38:43 -0500

    Generic Bet

---
+++ Betting/Betting/Bet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betting
+{
+	// T is your class implemenation of the PokerHand or Horse
+	// your class will implement ICompareable so it can be compared
+	public class Bet<T> where T : IComparable
+	{
+		public T Mine { get; set; }
+		public T[] Theirs { get; set; }
+
+		public int WagerAmount { get; set; }
+
+		public int DoBet()
+		{
+			if ( Mine == null || Theirs == null )
+			{
+				throw new InvalidOperationException( "Must define Mine and Theirs before betting" );
+			}
+			if ( Theirs.Length == 0 )
+			{
+				return WagerAmount;
+			}
+
+			T highest = Mine;
+			foreach ( T them in Theirs )
+			{
+				if ( highest.CompareTo( them ) <= 0 )
+				{
+					highest = them;
+				}
+			}
+
+			int numberOfHighest = 0;
+			if ( Mine.CompareTo(highest) == 0 )
+			{
+				numberOfHighest++;
+				foreach ( T them in Theirs )
+				{
+					if ( highest.CompareTo( them ) == 0 )
+					{
+						numberOfHighest++;
+					}
+				}
+
+				return ( WagerAmount * Theirs.Length + WagerAmount ) / numberOfHighest;
+			}
+			else
+			{
+				return 0;
+			}	
+		}
+	}
+}
---
+++ Betting/Betting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betting
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Bet<int> bet = new Bet<int>();
+
+			Random random = new Random();
+
+			bet.Mine = random.Next( 1, 100 );
+			bet.WagerAmount = 5;
+			bet.Theirs = new int[] {
+				random.Next( 1, 100 ),
+				random.Next( 1, 100 ),
+				random.Next( 1, 100 )
+			};
+
+			Console.WriteLine( $"I win: {bet.DoBet()}" );
+
+			Console.ReadKey();
+		}
+	}
+}
