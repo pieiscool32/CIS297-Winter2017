0f06930c1598a44fe893f44987477c3b5002cf9c
Eric Charnesky <eric.charnesky@careevolution.com>
2017-02-20 12:29:51 -0500

    Add crappy PokerHand class for example of CompareTo

---
+++ Betting/Betting/PokerHand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Betting
+{
+	public class PokerHand : IComparable<PokerHand>
+	{
+		public List<int> Cards { get; set; }
+
+		public PokerHand( List<int> cards )
+		{
+			Cards = cards;
+		}
+
+		public int CompareTo( PokerHand other )
+		{
+			return Cards[ 0 ].CompareTo( other.Cards[ 0 ] );
+		}
+	}
+}
--- Betting/Betting/Program.cs
+++ Betting/Betting/Program.cs
@@ -24,6 +24,19 @@ namespace Betting
 
 			Console.WriteLine( $"I win: {bet.DoBet()}" );
 
+
+			PokerHand hand1 = new PokerHand( new List<int> { 3, 2, 3, 4, 5 } );
+			PokerHand hand2 = new PokerHand( new List<int> { 2, 3, 4, 5, 6 } );
+
+			Bet<PokerHand> pokerBet = new Bet<PokerHand>();
+			pokerBet.WagerAmount = 10;
+			pokerBet.Mine = hand1;
+			pokerBet.Theirs = new[] { hand2 };
+
+
+			Console.WriteLine( $"Poker bet, I win? : {pokerBet.DoBet()}" );
+
+
 			Console.ReadKey();
 		}
 	}
